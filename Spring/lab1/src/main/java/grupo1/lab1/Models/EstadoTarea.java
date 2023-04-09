88bb2f22ce4f0ddbb47af237cd4ac7e9966761b2
Jhoisan Allendes <jhoisan.allendes@usach.cl>
2023-04-09 17:04:04 -0400

    Rename file

---
+++ Spring/lab1/src/main/java/grupo1/lab1/Models/EstadoTarea.java
@@ -0,0 +1,45 @@
+package grupo1.lab1.Models;
+
+/**
+ * Clase que representa una emergencia
+ */
+public class EstadoTarea{
+    private Integer id_Estado_tarea;
+    private String nombre_Estado_tarea;
+
+
+    /**
+     * Obtiene el id (integer) del estado de una tarea
+     * @return Integer
+     */
+    public Integer getId_Estado_tarea() {
+        return id_Estado_tarea;
+    }
+
+
+    /**
+     * Modifica un id (integer) a una Emergencia
+     * @param id_emergencia id de la emergencia
+     */
+    public void setId_Estado_tarea(Integer id_emergencia) {
+        this.id_Estado_tarea = id_Estado_tarea;
+    }
+
+
+    /**
+     * Obtiene el nombre (string) del estado de una tarea
+     * @return String
+     */
+    public String getNombre_Estado_tarea() {
+        return nombre_Estado_tarea;
+    }
+
+
+    /**
+     * Modifica el nombre (string) del estado de una tarea
+     */
+    public void setNombre_Estado_tarea(String nombre_Estado_tarea) {
+        this.nombre_Estado_tarea = nombre_Estado_tarea;
+    }
+
+}

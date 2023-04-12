fa0c0750259bd3bf1252daf44753531f2cc72495
Alan Donoso <Alan.Donoso.V@usach.cl>
2023-04-12 19:31:44 -0400

    Vol_habilidad arreglado (funcional en postman)

--- Spring/lab1/src/main/java/grupo1/lab1/Repositories/VoluntarioHabilidadRepositoryImp.java
+++ Spring/lab1/src/main/java/grupo1/lab1/Repositories/VoluntarioHabilidadRepositoryImp.java
@@ -1,5 +1,7 @@
 package grupo1.lab1.Repositories;
 
+import grupo1.lab1.Models.EmeHabilidad;
+import grupo1.lab1.Models.Voluntario;
 import grupo1.lab1.Models.VoluntarioHabilidad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,9 +18,9 @@ public class VoluntarioHabilidadRepositoryImp implements VoluntarioHabilidadRepo
     @Override
     public List<VoluntarioHabilidad> findAll() {
         List<VoluntarioHabilidad> voluntariosHabilidades;
-        String query = "SELECT * FROM VoluntarioHabilidad";
+        String query = "SELECT * FROM vol_habilidad";
         try (Connection conn = sql2o.open()) {
-            voluntariosHabilidades = conn.createQuery(query).setAutoDeriveColumnNames(true).executeAndFetch(VoluntarioHabilidad.class);
+            voluntariosHabilidades = conn.createQuery(query).executeAndFetch(VoluntarioHabilidad.class);
             return voluntariosHabilidades;
         } catch (Exception e) {
             System.out.println(e);
@@ -28,7 +30,7 @@ public class VoluntarioHabilidadRepositoryImp implements VoluntarioHabilidadRepo
     @Override
     public VoluntarioHabilidad findById(Integer id) {
         VoluntarioHabilidad voluntarioHabilidad;
-        String query = "SELECT * FROM VoluntarioHabilidad WHERE id_voluntario_habilidad = :id";
+        String query = "SELECT * FROM vol_habilidad WHERE id_vol_habilidad = :id";
         try (Connection conn = sql2o.open()) {
             voluntarioHabilidad = conn.createQuery(query)
                     .addParameter("id", id)
@@ -39,15 +41,47 @@ public class VoluntarioHabilidadRepositoryImp implements VoluntarioHabilidadRepo
         }
         return null;
     }
+
+    @Override
+    public List<VoluntarioHabilidad> findById_Habilidad(Integer id_habilidad) {
+        List<VoluntarioHabilidad> encontrada;
+        String query = "SELECT * FROM vol_habilidad WHERE id_habilidad = :id_habilidad";
+        try (Connection conn = sql2o.open()) {
+            encontrada = conn.createQuery(query)
+                    .addParameter("id_habilidad", id_habilidad)
+                    .executeAndFetch(VoluntarioHabilidad.class);
+            return encontrada;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    @Override
+    public List<VoluntarioHabilidad> findById_Voluntario(Integer id_voluntario) {
+        List<VoluntarioHabilidad> encontrada;
+        String query = "SELECT * FROM Vol_habilidad WHERE id_voluntario = :id";
+        try (Connection conn = sql2o.open()) {
+            encontrada = conn.createQuery(query)
+                    .addParameter("id", id_voluntario)
+                    .executeAndFetch(VoluntarioHabilidad.class);
+            return encontrada;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
     @Override
     public VoluntarioHabilidad save(VoluntarioHabilidad VoluntarioHabilidad){
-        String query = "INSERT INTO VoluntarioHabilidad (id_voluntario_habilidad) VALUES (:id_voluntario_habilidad)";
+        String query = "INSERT INTO vol_habilidad (id_voluntario, id_habilidad) VALUES (:id_voluntario, :id_habilidad)";
         try (Connection conn = sql2o.open()) {
             Integer id = (int) conn.createQuery(query, true)
-                    .addParameter("id_voluntario_habilidad", VoluntarioHabilidad.getId_voluntario_habilidad())
+                    .addParameter("id_voluntario", VoluntarioHabilidad.getId_Voluntario())
+                    .addParameter("id_habilidad", VoluntarioHabilidad.getId_Habilidad())
                     .executeUpdate()
                     .getKey();
-            VoluntarioHabilidad.setId_voluntario_habilidad(id);
+            VoluntarioHabilidad.setId_vol_habilidad(id);
             return VoluntarioHabilidad;
         } catch (Exception e) {
             System.out.println(e);
@@ -55,14 +89,16 @@ public class VoluntarioHabilidadRepositoryImp implements VoluntarioHabilidadRepo
         return null;
     }
     @Override
-    public void delete(Integer id) {
-        String query = "DELETE FROM VoluntarioHabilidad WHERE id_voluntario_habilidad = :id";
+    public boolean delete(Integer id) {
+        String query = "DELETE FROM vol_habilidad WHERE id_vol_habilidad = :id";
         try (Connection conn = sql2o.open()) {
             conn.createQuery(query)
                     .addParameter("id", id)
                     .executeUpdate();
+            return true;
         } catch (Exception e) {
             System.out.println(e);
+            return false;
         }
     }
 }
--- Spring/lab1/src/main/java/grupo1/lab1/Services/VoluntarioHabilidadService.java
+++ Spring/lab1/src/main/java/grupo1/lab1/Services/VoluntarioHabilidadService.java
@@ -1,6 +1,7 @@
 package grupo1.lab1.Services;
 
 import grupo1.lab1.Models.Emergencia;
+import grupo1.lab1.Models.TareaHabilidad;
 import grupo1.lab1.Models.VoluntarioHabilidad;
 import grupo1.lab1.Repositories.VoluntarioHabilidadRepository;
 import org.springframework.web.bind.annotation.*;
@@ -31,17 +32,49 @@ public class VoluntarioHabilidadService {
             return null;
         }
     }
+
+    @GetMapping("/voluntarioHabilidad/id_habilidad/{id_habilidad}")
+    public List<VoluntarioHabilidad> findById_Habilidad(@PathVariable("id_habilidad") String id_str){
+        Integer id;
+        try{
+            id = Integer.parseInt(id_str);
+            return voluntarioHabilidadRepository.findById_Habilidad(id);
+        }catch(Exception e){
+            System.out.println("Ingrese un id valido");
+        }
+        return null;
+    }
+
+    @GetMapping("/voluntarioHabilidad/id_voluntario/{id_voluntario}")
+    public List<VoluntarioHabilidad> findById_Voluntario(@PathVariable("id_voluntario") String id_str){
+        Integer id;
+        try{
+            id = Integer.parseInt(id_str);
+            return voluntarioHabilidadRepository.findById_Voluntario(id);
+        }catch(Exception e){
+            System.out.println("Ingrese un id valido");
+        }
+        return null;
+    }
+
     @PostMapping("/voluntarioHabilidad")
     @ResponseBody
     public VoluntarioHabilidad createVoluntarioHabilidad(@RequestBody VoluntarioHabilidad voluntarioHabilidad){
         return voluntarioHabilidadRepository.save(voluntarioHabilidad);
     }
+
     @DeleteMapping("/voluntarioHabilidad/{id}")
     public void deleteVoluntarioHabilidad(@PathVariable("id") String id_str){
         Integer id;
         try{
             id = Integer.parseInt(id_str);
-            voluntarioHabilidadRepository.delete(id);
+
+            if (voluntarioHabilidadRepository.delete(id)) {System.out.println("Fila eliminada con exito");
+
+            }else{
+                System.out.println("La fila no fue eliminada");
+            }
+
         }catch (Exception e){
             System.out.println("Ingrese un id valido");
         }

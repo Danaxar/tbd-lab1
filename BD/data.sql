6f09dc3f0299c97b33f0140b31e50b5997c10c4e
Daniel <daniel.catalan@usach.cl>
2023-04-17 11:53:45 -0400

    feat: data script bd

---
+++ BD/data.sql
@@ -0,0 +1,23 @@
+insert into
+    habilidad (nombre_habilidad, puntaje_habilidad)
+values
+    ('Fuerza física', 8),
+    ('Equipamiento', 7),
+    ('Conocimiento en primeros auxilios', 9),
+    ('Experiencia en atención al cliente', 7),
+    ('Habilidad en cocina', 6),
+    ('Conocimiento en informática', 8),
+    ('Conocimiento en electricidad', 8),
+    ('Conocimiento del terreno', 9),
+    ('Habilidad de traducción', 7),
+    ('Habilidad de manejo de animales', 8),
+    ('Conocimiento de construcción', 9),
+    ('Conocimientos de medicina', 10);
+
+insert into
+    institucion (nombre_institucion)
+values
+    ('Cruz Roja'),
+    ('Bomberos'),
+    ('Carabineros'),
+    ('Armada');

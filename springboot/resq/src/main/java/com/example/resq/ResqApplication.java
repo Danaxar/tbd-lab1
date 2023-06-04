855975fde6330a9852fc18077563758ee683a93f
Daniel <daniel.catalan@usach.cl>
2023-06-03 20:52:57 -0400

    feat: new spring proyect

---
+++ springboot/resq/src/main/java/com/example/resq/ResqApplication.java
@@ -0,0 +1,13 @@
+package com.example.resq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResqApplication.class, args);
+	}
+
+}

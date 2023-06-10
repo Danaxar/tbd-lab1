77c1d0fdcfe14a772d69b201c61c7b73c4672916
Daniel <daniel.catalan@usach.cl>
2023-06-10 17:58:05 -0400

    feat: map

--- resq/pages/home.vue
+++ resq/pages/home.vue
@@ -4,15 +4,18 @@
     <div class="container">
       <!-- Contenido de la página principal -->
       <h1>Pagina principal</h1>
+      <Mapa></Mapa>
     </div>
   </div>
 </template>
 
 <script>
+import Mapa from '~/components/Mapa.vue'
 import NavBar from '../components/NavBar.vue'
 export default {
   components: {
     NavBar,
+    Mapa,
   },
 }
 </script>
